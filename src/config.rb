bd9e2481a0a499e134e6debeb731576e8f477e7c
elliottomlinson <elliottomlinson@gmail.com>
2021-03-09 14:02:55 -0500

    created art folder

--- src/config.rb
+++ src/config.rb
@@ -1,3 +1,11 @@
-# local directory paths
-GIT_DIR = "/home/ellio/projects/rpg-art"
+# tabletop simulator saved object folder
 SAVED_OBJ_DIR = "/home/ellio/.local/share/Tabletop Simulator/Saves/Saved Objects"
+
+# local
+GIT_DIR = File.expand_path("..", Dir.pwd)
+
+
+# figurine import
+CHAR_GIT_DIR = GIT_DIR+"/art/char"
+CHAR_SAVED_DIR = SAVED_OBJ_DIR+"/char"
+CHAR_URL = "https://github.com/elliottomlinson/rpg-art/blob/master/art/char/%{set}/%{name}?raw=true"
--- src/templates.rb
+++ src/templates.rb
@@ -1,10 +1,5 @@
 require_relative "./config.rb"
 
-# figurine import
-CHAR_GIT_DIR = GIT_DIR+"/char"
-CHAR_SAVED_DIR = SAVED_OBJ_DIR+"/char"
-CHAR_URL = "https://github.com/elliottomlinson/rpg-art/blob/master/char/%{set}/%{name}?raw=true"
-
 # i'm certain this is a major rookie move
 CHARFILE_OPEN = "{\"ObjectStates\":["
 CHARFILE_ENTRY = "
