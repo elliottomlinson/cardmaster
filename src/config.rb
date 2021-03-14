601cb135a00f352dbc47c4e575d08e3661a8271a
elliottomlinson <elliottomlinson@gmail.com>
2021-03-14 00:42:59 -0500

    windows test

--- src/config.rb
+++ src/config.rb
@@ -1,9 +1,9 @@
 # tabletop simulator saved object folder
-SAVED_OBJ_DIR = "/home/ellio/.local/share/Tabletop Simulator/Saves/Saved Objects"
+SAVED_OBJ_DIR = "C:\Users\ellio\Documents\My Games\Tabletop Simulator\Saves\Saved Objects"
 
 # local
 # will fix this to be compatible with all machines
-GIT_DIR = "/home/ellio/projects/rpg-art"
+GIT_DIR = "C:\Users\ellio\projects\rpg-art"
 
 # figurine import
 CHAR_GIT_DIR = GIT_DIR+"/art/char"
