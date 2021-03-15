352165be650c624b4b79209e3dcf5a442843f9e0
elliottomlinson <elliottomlinson@gmail.com>
2021-03-15 14:57:12 -0400

    planning-related annotations to conig

--- src/config.rb
+++ src/config.rb
@@ -1,17 +1,18 @@
 # tabletop simulator saved object folder
+# condsider implementing an option to choose if you're elliot or duncan
 SAVED_OBJ_DIR = "/home/ellio/.local/share/Tabletop Simulator/Saves/Saved Objects"
 
 # local
-# will fix this to be compatible with all machines
-GIT_DIR = "/home/ellio/projects/rpg-art"
+# will fix this to be compatible cross-platform
+GIT_DIR = "/home/ellio/projects/rpcg"
 
 # figurine import
 CHAR_GIT_DIR = GIT_DIR+"/art/char"
 CHAR_SAVED_DIR = SAVED_OBJ_DIR+"/char"
-CHAR_URL = "https://github.com/elliottomlinson/rpg-art/blob/master/art/char/%{set}/%{name}?raw=true"
+CHAR_URL = "https://github.com/elliottomlinson/rpcg/blob/master/art/char/%{set}/%{name}?raw=true"
 
 # item import
 ITEM_GIT_DIR = GIT_DIR+"/art/item"
 ITEM_SAVED_DIR = SAVED_OBJ_DIR+"/item"
-ITEM_FACE_URL = "https://github.com/elliottomlinson/rpg-art/blob/master/art/item/%{set}/%{name}?raw=true"
-ITEM_BACK_URL = "https://github.com/elliottomlinson/rpg-art/blob/master/art/item/back/%{set}.png?raw=true"
+ITEM_FACE_URL = "https://github.com/elliottomlinson/rpcg/blob/master/art/item/%{set}/%{name}?raw=true"
+ITEM_BACK_URL = "https://github.com/elliottomlinson/rpcg/blob/master/art/item/back/%{set}.png?raw=true"
