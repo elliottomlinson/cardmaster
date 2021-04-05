1da114d99d3c8e985e05b3f1425297bc3b6b080b
Duncan Uszkay <duncan.uszkay@gmail.com>
2021-04-05 13:11:44 -0400

    Added a JSON catalogue adapter

--- src/card/generate_card_assets.rb
+++ src/card/generate_card_assets.rb
@@ -1,5 +1,12 @@
 require_relative "./models.rb"
 require_relative "./generators.rb"
+require_relative "./catalogue_adapters.rb"
+
+catalogue_adapter = Card::CatalogueAdapters::JsonCatalogueAdapter.new("data/card_catalogue.json")
+
+puts catalogue_adapter.printable_cards
+
+return
 
 generator = Card::Generators::VipsBasic.new
 
