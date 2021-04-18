fa0db2a494377ab5e12c1f36b1c49da7c56ab274
Duncan Uszkay <duncan.uszkay@gmail.com>
2021-04-18 14:59:54 -0400

    tmove directories into base script

--- src/import-cards.rb
+++ src/import-cards.rb
@@ -2,7 +2,11 @@ require "bundler/setup"
 require_relative "tabletop_simulator/importers/card_importer.rb"
 require_relative "tabletop_simulator/models/card.rb"
 
-card_importer = TabletopSimulator::Importers::CardImporter.new
+# todo centralize
+saved_objects_folder = "/Users/duncanuszkay/Library/Tabletop Simulator/Saves/Saved Objects"
+base_directory = "Cardmaster"
+
+card_importer = TabletopSimulator::Importers::CardImporter.new(saved_objects_folder, base_directory)
 
 card = TabletopSimulator::Models::Card.from_template
 
--- src/tabletop_simulator/importers/card_importer.rb
+++ src/tabletop_simulator/importers/card_importer.rb
@@ -2,11 +2,15 @@ require "fileutils"
 
 module TabletopSimulator
   # todo: centralize this
-  TABLETOP_SAVE_OBJECTS = "/Users/duncanuszkay/Library/Tabletop Simulator/Saves/Saved Objects"
   module Importers
     class CardImporter
-      BASE_PATH = %w(cardmaster cards).freeze
-      INDIVIDUAL_CARD_PATH = BASE_PATH + %w(individual)
+      BASE_PATH = %w(Cards).freeze
+      INDIVIDUAL_CARDS_PATH = (BASE_PATH + %w(Individual)).freeze
+
+      def initialize(saved_objects_folder, base_directory)
+        @saved_objects_folder = saved_objects_folder
+        @base_directory = base_directory
+      end
 
       def import(card)
         save_individual_card(card)
@@ -15,15 +19,19 @@ module TabletopSimulator
       private
 
       def save_individual_card(card)
-        save_card(card, INDIVIDUAL_CARD_PATH)
+        save_card(card, INDIVIDUAL_CARDS_PATH)
       end
 
       def save_card(card, folders)
-        dir_path = File.join(folders.prepend(TABLETOP_SAVE_OBJECTS))
+        dir_path = path_in_saved_objects(folders)
         FileUtils.mkdir_p(dir_path)
 
         path = File.join(dir_path, "#{card.name}.json")
         File.write(path, card.to_json)
+      end
+
+      def path_in_saved_objects(folders)
+        File.join(@saved_objects_folder, @base_directory, folders)
       end
     end
   end
