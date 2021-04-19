741bb0db1f8a99df58575f12b3eddecf157edd73
Duncan Uszkay <duncan.uszkay@gmail.com>
2021-04-18 23:01:04 -0400

    Split printed into storage

--- src/card/storage_adapters/git_storage_adapter.rb
+++ src/card/storage_adapters/git_storage_adapter.rb
@@ -10,37 +10,43 @@ module Card
       PRINT_MANIFEST_PATH = "assets/core/card/printed/print_manifest.json"
       GH_URL = "https://raw.githubusercontent.com/elliottomlinson/rpcg"
 
-      def save(cards)
-        cards.each do |card|
+      def save(printed_cards)
+        printed_cards.each do |printed_card|
           FileUtils.cp(
-            card.image_path,
-            card_file_name(card)
+            printed_card.image_path,
+            card_file_name(printed_card)
           )
         end
 
-        update_manifest(cards)
+        update_manifest(printed_cards)
       end
 
       def printed_cards
-        raise "Git Print Manifest unavailable, try regenerating assets" unless File.exists?(PRINT_MANIFEST_PATH)
+        raise "Git Print Manifest unavailable, try printing again" unless File.exists?(PRINT_MANIFEST_PATH)
 
-        Marshal.load(File.read(PRINT_MANIFEST_PATH)).each do |printed_card|
-          printed_card.image_url = github_repo_url(printed_card)
-        end
+        stored_cards = Marshal.load(File.read(PRINT_MANIFEST_PATH))
+
+        raise "Print Manifest Invalid, try printing again" unless stored_cards[0].is_a?(Card::Models::StoredCard)
+
+        stored_cards
       end
 
       private
 
-      def update_manifest(cards)
-        File.write(PRINT_MANIFEST_PATH, Marshal.dump(cards))
+      def update_manifest(printed_cards)
+        stored_cards = printed_cards.map do |printed_card|
+          Card::Models::StoredCard.new(printed_card.spec, github_repo_url(printed_card))
+        end
+
+        File.write(PRINT_MANIFEST_PATH, Marshal.dump(stored_cards))
       end
 
-      def card_file_name(card)
-        File.join(ASSET_PATH, "#{card.spec.title}.png")
+      def card_file_name(printed_card)
+        File.join(ASSET_PATH, "#{printed_card.spec.title}.png")
       end
 
-      def github_repo_url(card)
-        "#{GH_URL}/master/#{card_file_name(card)}"
+      def github_repo_url(printed_card)
+        "#{GH_URL}/master/#{card_file_name(printed_card)}"
       end
     end
   end
