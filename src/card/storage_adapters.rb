3389e9dd9389cee3609b4764e6bbba174fa16e1a
Duncan Uszkay <duncan.uszkay@gmail.com>
2021-05-08 15:20:07 -0400

    Update abstract

--- src/card/storage_adapters.rb
+++ src/card/storage_adapters.rb
@@ -3,15 +3,20 @@ module Card
     class Abstract
       # Takes a list of Card::Models::PrintedCards
       # stores the images in a location which can be
-      # loaded via the load method
+      # retrieved via the stored_cards method
       def save(cards)
         raise UnimplementedError
       end
 
-      # Takes a list of Card::Models::CardSpecification
-      # Returns a list of tuples matching CardSpecifications
-      # with resource URLs
-      def load(cards)
+      # Retrieve a list of Card::Models::StoredCards,
+      # one for each card stored by this adapter
+      def stored_cards(cards)
+        raise UnimplementedError
+      end
+
+      # Request that a card with the given title be deleted.
+      # It will not be returned by stored_cards afterwards.
+      def delete(title)
         raise UnimplementedError
       end
     end
